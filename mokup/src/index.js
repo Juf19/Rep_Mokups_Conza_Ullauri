0a8989bcafc378a9aec0f4284b5adc62091a14c4
Juf19 <jorgeufer.19@gmail.com>
2024-11-26 19:51:23 -0500

    actualizar App4.css

--- mokup/src/index.js
+++ mokup/src/index.js
@@ -9,15 +9,15 @@ import './index.css';
 //import App5 from './App5';
 //import App6 from './App6';
 //import App7 from './App7';
-//import App8 from './App8';
-import App9 from './App9';
+import App8 from './App8';
+//import App9 from './App9';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <App9 />
+    <App8 />
   </React.StrictMode>
 );
 
